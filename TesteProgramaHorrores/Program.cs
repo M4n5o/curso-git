e29625f74fcc79561edca45e0081623d43194e49
Fernando Vieira <fermanso.git@gmail.com>
2020-12-26 15:15:54 -0300

    Programa imprime mensagem Olá caraio!

---
+++ TesteProgramaHorrores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteProgramaHorrores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá caraio!");
+        }
+    }
+}
